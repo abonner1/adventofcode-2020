6a5294cc90da6693e5dde935d849b4e12336a44f
Andrew Bonner <abonner@covermymeds.com>
2020-12-01 12:51:19 -0500

    finish solution

--- one/main.rb
+++ one/main.rb
@@ -1,1 +1,28 @@
-number_list = File.readlines('input.txt', chomp: true)
+number_list = File.readlines('input.txt', chomp: true).collect(&:to_i)
+
+def two_numbers(number_list)
+  return if number_list.empty?
+  number = number_list[0]
+  number_list = number_list.drop(1)
+  number_list.each_with_index do | v, i |
+    if i < number_list.length - 1
+      return number * v if number + v == 2020
+    end
+  end
+  two_numbers(number_list)
+end
+
+def three_numbers(number_list)
+  return if number_list.empty?
+
+  number = number_list[0]
+  number_list = number_list.drop(1)
+
+  number_list.each_with_index do |v, i|
+    number_list.drop(1).each_with_index do |v2, i2|
+      return number * v * v2 if number + v + v2 == 2020
+    end
+  end
+
+  three_numbers(number_list)
+end
